d1e7ed8184667c846e3bec57e335615a02714f86
BilousPM <bilouspm@gmail.com>
2024-10-06 06:22:48 -0600

    add  user register

---
+++ src/db/models/User.js
@@ -0,0 +1,24 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      unique: true,
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+export const UserCollection = model('user', userSchema);
